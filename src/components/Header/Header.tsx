599e1167889324259fdfa9ed8839d2b50011f770
PaulGreetham <pgreetham@protonmail.com>
2024-08-27 21:21:18 +0200

    light and dark icons created

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './Header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFutbol } from '@fortawesome/free-solid-svg-icons';
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
-
-
+import { faFutbol, faMoon, faSun, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import useTheme from '../../hooks/useTheme';
 
 const Header: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <header className="header">
       <div className="header-container">
         <div className="logo">
-          <FontAwesomeIcon icon={faFutbol} className="icon"/>
+          <FontAwesomeIcon icon={faFutbol} className="logo-icon" />
         </div>
-        <div className="auth-buttons">
-          <FontAwesomeIcon icon={faRightFromBracket} className="icon" />       </div>
+        <div className="header-buttons">
+          <button onClick={toggleTheme} className="theme-toggle">
+            <FontAwesomeIcon icon={theme === 'light' ? faMoon : faSun} className="icon" />
+          </button>
+          <button className="auth-buttons">
+            <FontAwesomeIcon icon={faRightFromBracket} className="icon" />
+          </button>
+        </div>
       </div>
     </header>
   );
---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+const useTheme = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme as 'light' | 'dark');
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return { theme, toggleTheme };
+};
+
+export default useTheme;
